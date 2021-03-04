b3af54bcaf5ab2abaa174cf0a801338744f5a8bf
MaxenceLenoir <maxence.lenoir1206@gmail.com>
2021-03-04 09:42:33 +0100

    Input text change

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,11 +1,22 @@
 import { Controller } from 'stimulus'; 
 export default class extends Controller {
-  static targets = ["heading"]
+  static targets = ["heading", "paraph", "input", "text"]
 
   connect() {
     console.log("hello from StimulusJS")
   }
   greet() {
     this.headingTarget.innerHTML = "Hello World"
+  }
+  toogle() {
+    if(this.paraphTarget.innerHTML === "Je suis un paragraph"){
+      this.paraphTarget.innerHTML = "Je ne suis plus un paragraph"
+    } else {
+      this.paraphTarget.innerHTML = "Je suis un paragraph"
+    }
+  }
+
+  inputChange() {
+    this.textTarget.innerHTML = this.inputTarget.value
   }
 }
